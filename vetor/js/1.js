6d2b0d93e814d1bbbb0709046d5afa646cdb5125
Carol cristine <carolaynecabral2005@gmail.com>
2023-03-23 18:37:33 -0300

    Aula

---
+++ vetor/js/1.js
@@ -0,0 +1,20 @@
+function montarVetor() {
+    const B = [];
+    for (let index = 0; index < 1; index++) { //index++ <=> index=index+1
+        let valor = Math.floor(Math.random() * 30) + 1;
+        if (index % 2 === 0) {
+            //B[index] = 0;
+            if (valor % 2 != 0) {
+                valor++
+            }
+            B[index] = valor;
+        } else {
+            //B[index] = 1;
+            if (valor % 2 == 0) {
+                valor++
+            }
+            B[index] = valor;
+        }
+    }
+    document.querySelector("#saidaTexto").innerHTML = B;
+}
